6784890e55f8168337ab1e66ebc0d48710f7dee0
Santiago Saavedra <santiago.saavedra@zemoga.com>
2023-08-09 10:46:18 -0500

    High Order Component Pattern

---
+++ React/High Order Component Pattern/HOC.tsx
@@ -0,0 +1,12 @@
+export function withStyles(Component) {
+  return (props) => {
+    const style = {
+      color: "red",
+      fontSize: "1em",
+      // Merge props
+      ...props.style,
+    };
+
+    return <Component {...props} style={style} />;
+  };
+}
